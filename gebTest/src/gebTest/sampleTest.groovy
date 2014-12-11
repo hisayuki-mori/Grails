ebbf53a658b64a1a912f9b458254168c2cc9ee43
hisayuki-mori <siruvalice@gmail.com>
2014-12-12 00:43:56 +0900

    test

--- gebTest/src/gebTest/sampleTest.groovy
+++ gebTest/src/gebTest/sampleTest.groovy
@@ -1,18 +1,17 @@
 package gebTest;
 
 @Grapes([
-    @Grab("org.codehaus.geb:geb-core:latest.release"),
+    @Grab("org.gebish:geb-core:latest.release"),
+	@Grab("org.gebish:geb-spock:latest.release"),
 	@Grab("org.seleniumhq.selenium:selenium-ie-driver:latest.release")
 ])
 
-
 import geb.Browser
-
 import org.junit.Test;
+
+System.setProperty("webdriver.ie.driver", "driver/IEDriverServer.exe")
 
 Browser.drive {
 	go 'https://google.co.jp'
-	$('input[type=text]').value('groovy')
-	assert $('input[type=text]').value() == 'groovy'
+	$('input[type=text]').value('クックパッド')
 }
-
